1e7c781ee4327a0458af1471a41792257597d955
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-18 17:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
-    var total = total()
+    var sum = total()
     cart = []
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
   }
   else {
     return "Sorry, we don\'t have a credit card on file for you."
