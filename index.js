60d5a2f723822711b57eabb55c30975bba3cebdb
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-18 17:12:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,4 +69,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber) {
+    var total = total()
+    cart = []
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }
+  else {
+    return "Sorry, we don\'t have a credit card on file for you."
+  }
 }
